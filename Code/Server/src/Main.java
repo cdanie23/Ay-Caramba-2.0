ae7a380b82b94fc7647147e004f33671d42bcce0
cdanie23 <cdanie23@my.westga.edu>
2025-11-17 13:32:19 -0500

    Implements Colby's tasks of the project still missing logging and minor testing

--- Code/Server/src/Main.java
+++ Code/Server/src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import library.lib;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * The entry point to the program
+ */
+public class Main {
+    private static final String DATA_FOLDER = "Data";
+    private static final int NUM_OF_THREADS = 4;
+    public static void main(String[] args) {
+        File dataFolder = new File(DATA_FOLDER);
+        if (!dataFolder.exists()) {
+            dataFolder.mkdir();
+        }
+        ExecutorService threadPool = Executors.newFixedThreadPool(NUM_OF_THREADS);
+    }
+
+    private void start_listening(ExecutorService threadPool) {
+
+        try (ServerSocket socket = new ServerSocket(1027)) {
+            while (true) {
+                Socket client = socket.accept();
+                threadPool.execute(() -> {
+                    lib.handleClient(client);
+                });
+            }
+        } catch (Exception exception) {
+            System.err.println(exception.getMessage());
         }
     }
 }
---
+++ Code/Server/src/library/lib.java
@@ -0,0 +1,59 @@
+package library;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+/**
+ * The library class where all our utility functions reside
+ * @author Colby
+ */
+public class lib {
+
+    /**
+     * Handles the client connection
+     * @param client the socket to the client connection
+     */
+    public static void handleClient(Socket client) {
+        System.out.println("Server:: Client's IP is -> " + client.getInetAddress().getHostAddress());
+
+        try {
+            //TODO El's part of the server program goes here
+            // NOTE FOR EL: the working directory of this module i.e the server program is
+            // Server/.... whatever you want to create a path to
+            // that is where the start is for all paths in the server module
+            String pathToBranchFolderCreated = null;
+            // change the above just a placeholder for me
+
+            String base64EncodedString = client.getOutputStream().toString();
+            System.out.println("Server:: Base64 encoded message -> " + base64EncodedString);
+            PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+            pout.println("OK");
+            client.close();
+            processData(base64EncodedString, pathToBranchFolderCreated);
+        } catch (Exception exception) {
+            System.err.println(exception.getMessage());
+        }
+    }
+
+    private static void processData(String base64EncodedString, String branchFolderPath) {
+        String trimmedData = base64EncodedString.replaceAll("~", "");
+        String data = decodeBase64(trimmedData);
+        System.out.println("Server:: Data received -> " + data);
+        File weekly_sales = new File(branchFolderPath + "/branch_weekly_sales.txt");
+        try (FileWriter fw = new FileWriter(weekly_sales, false)) {
+            fw.write(data);
+        } catch (IOException exception) {
+            System.err.println(exception.getMessage());
+        }
+    }
+
+    private static String decodeBase64(String base64EncodedString) {
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] decodedBytes = decoder.decode(base64EncodedString);
+        return new String(decodedBytes, StandardCharsets.UTF_8);
+    }
+}
